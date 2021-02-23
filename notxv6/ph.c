1c126405de0607db5d677de32422e2ac87012ae5
za <2356184200@qq.com>
2021-02-23 17:25:23 +0800

    lab7 done

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -15,6 +15,7 @@ struct entry {
 };
 struct entry *table[NBUCKET];
 int keys[NKEYS];
+pthread_mutex_t locks[NBUCKET];
 int nthread = 1;
 
 double
@@ -42,6 +43,7 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  pthread_mutex_lock(locks + i);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -53,6 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(locks + i);
 }
 
 static struct entry*
@@ -62,9 +65,11 @@ get(int key)
 
 
   struct entry *e = 0;
+  pthread_mutex_lock(locks + i);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+  pthread_mutex_unlock(locks + i);
 
   return e;
 }
@@ -102,11 +107,13 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
-
+  
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
   }
+  for (int i = 0; i < NBUCKET; ++i)
+      pthread_mutex_init(locks + i, NULL);
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
